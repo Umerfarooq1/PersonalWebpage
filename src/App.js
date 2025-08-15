2914f8e3bf105aabc5fd30c9175f81acd4ebd23e
Umer Farooq <46781582+Umerfarooq1@users.noreply.github.com>
2025-08-15 12:00:27 -0500

    Add React src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, I'm Umer Farooq</h1>
+      <p>Welcome to my personal webpage!</p>
+    </div>
+  );
+}
+
+export default App;
